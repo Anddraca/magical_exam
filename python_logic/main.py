a844be557115fcdb1087bd995a4e1013c60af694
eltomo4 <eltomo4@gmail.com>
2018-01-10 20:52:10 -0500

    added py entry

---
+++ python_logic/main.py
@@ -0,0 +1,16 @@
+"""
+Entry point file. Will call in other elements needed, execute main logic
+
+Author: Tom Anderson 
+
+&copy 2018
+
+"""
+
+import json
+# get the card data
+monsters_parsed = json.load(open('data/monsterCards.json'))
+
+# print(monsters_parsed)
+
+
