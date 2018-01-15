991f97d0cd5b472f772efc5937c4f64da24d8376
eltomo4 <eltomo4@gmail.com>
2018-01-15 18:40:58 -0500

    fixing error in json

--- python_logic/main.py
+++ python_logic/main.py
@@ -13,4 +13,31 @@ monsters_parsed = json.load(open('data/monsterCards.json'))
 
 # print(monsters_parsed)
 
+# define a generic card class:
 
+class Card:
+    """
+    Define generic card:
+
+    Type: String - Monster or Spell
+    ID: String - the ID of the card
+    Name: String - Name of the card
+    """
+
+    type = ""
+    id = ""
+    name = ""
+
+    def get_info_by_id(self, id):
+        card_info = monsters_parsed[id]
+        return card_info
+
+# test out the thing:
+
+users_id = input("ID of card you want to check out:")
+
+new_card = Card()
+
+this_card_info = new_card.get_info_by_id(users_id)
+
+print(this_card_info)
