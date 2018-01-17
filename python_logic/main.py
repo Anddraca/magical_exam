006c15e4af9fc0f7262ce2bb9f611e2f843bdf9f
tand44 <tom.math.anderson@gmail.com>
2018-01-17 11:51:31 -0500

    started structure for looping prompt

--- python_logic/main.py
+++ python_logic/main.py
@@ -1,7 +1,7 @@
 """
 Entry point file. Will call in other elements needed, execute main logic
 
-Author: Tom Anderson 
+Author: Tom Anderson
 
 &copy 2018
 
@@ -29,8 +29,12 @@ class Card:
     name = ""
 
     def get_info_by_id(self, id):
-        card_info = monsters_parsed[id]
-        return card_info
+        try:
+            card_info = monsters_parsed[id]
+            return card_info
+        except KeyError:
+            print("Not a valid ID, please try again")
+            return None
 
 
 class MonsterCard(Card):
@@ -41,15 +45,38 @@ class MonsterCard(Card):
     attack = ""
     hp = ""
 
+class playerHand:
+    """
+    is a list of cards (by id) in the player's hand
+    """
+
+    cards_in_hand = []
+
+    def add_to_hand(self, id):
+        self.cards_in_hand.append(id)
 
 # test out the thing:
 
-users_id = input("ID of card you want to check out:")
+print("Enter q to quit")
+command = input("What would you like to do next?")
+
+def handle_commands(command):
+    if(command == "M101"):
+        print("That is card M101")
+    else:
+        print("Command not recognized, please try again")
+
+while command != "q":
+    handle_commands(command)
+    command = input("What would you like to do next?")
+
+# users_id = input("ID of card you want to check out:")
 
 new_card = Card()
 
-this_card_info = new_card.get_info_by_id(users_id)
-formatted_info = "Name: " + this_card_info['name'] + "\nHP: " + this_card_info['hp'] + "\nAttack: " + this_card_info['attack'] + "\nLevel: " + this_card_info['level'] + "\nFlavor Text: " + this_card_info['flavorText'] + "\nText: " + this_card_info['text']
+# this_card_info = new_card.get_info_by_id(users_id)
+formatted_info = ""
 
-
-print(formatted_info)
+#if this_card_info is not None:
+ #   formatted_info = "Name: " + this_card_info['name'] + "\nHP: " + this_card_info['hp'] + "\nAttack: " + this_card_info['attack'] + "\nLevel: " + this_card_info['level'] + "\nFlavor Text: " + this_card_info['flavorText'] + "\nText: " + this_card_info['text']
+  #  print(formatted_info)
